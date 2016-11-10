952c9c6917b40e27636c8b31802538d446f1de98
subramaniashiva <vengaishiva@gmail.com>
2016-11-10 22:30:49 +0000

    show/hide uncompleted tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,8 +6,23 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false
+    }
+  }
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
   renderTasks() {
-    return this.props.tasks.map(task => (
+    let filteredTasks = this.props.tasks;
+    if(this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map(task => (
       <Task key={task._id} task={task}></Task>));
   }
   handleSubmit(event) {
@@ -22,6 +37,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <label>
+          <input type='checkbox' readOnly checked={this.state.hideCompleted}
+          onClick={this.toggleHideCompleted.bind(this)} />
+          Hide Completed Tasks
+        </label>
         <form onSubmit={this.handleSubmit.bind(this)}>
           <input type='text' placeholder='enter to do' ref='textInput' />
         </form>
